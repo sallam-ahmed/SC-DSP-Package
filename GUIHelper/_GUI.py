0e7cf0518b80faeb1a73c317b019d3b436bebb86
Ahmed Yehia GPEL <ahmed.yehia.1177@gmail.com>
2018-03-27 19:57:10 +0200

    Added DrawAxisArray, DrawCheckBox wrappers

--- GUIHelper/_GUI.py
+++ GUIHelper/_GUI.py
@@ -64,6 +64,22 @@ class GUI:
                                 columnspan = columnSpan, rowspan = rowSpan,
                                 sticky= sticky)
                 rootApp.controls[name] = newControl
+
+        @staticmethod
+        def DrawCheckBox(rootApp,variable,text = 'text', name = "ch1",position = (10,10),
+                         padX = 0, padY = 0,
+                         sticky = (tk.W,tk.E), columnSpan = 1, rowSpan = 1,
+                         owner = None):
+                if owner == None:
+                        owner = rootApp
+                        
+                newControl = tk.Checkbutton(owner,text = text, variable = variable)
+                newControl.grid(row = position[0],column = position[1],
+                                padx = padX,pady = padY,
+                                columnspan = columnSpan, rowspan = rowSpan,
+                                sticky= sticky)
+                rootApp.controls[name] = newControl
+
         @staticmethod
         def DrawGroupBox(rootApp,name = "GroupBox1", text = "GroupBox1",
                            position = (10,10),
@@ -157,7 +173,38 @@ class GUI:
                 canvas.show()
                 rootApp.signalPlotters[name] = (ax,canvas,fig)
                 rootApp.controls[name] = canvas.get_tk_widget()
-     
+        
+        @staticmethod
+        def DrawAxisArray(rootApp, name = "SourcePlot",
+                       text = "Signal View'",
+                       xLabel = "time (s)",
+                        yLabel = "voltage (mV)",
+                       position = (10,10),
+                       padX = 0, padY = 0,
+                       sticky = (tk.S,tk.W), columnSpan = 1, rowSpan = 1,
+                       owner = None):
+                if owner == None:
+                        owner = rootApp
+                fig, axis_array = plt.subplots(2,2)
+                plt.tight_layout()
+                
+                for i in range(2):
+                        for j in range(2):
+                                axis_array[i,j].grid('on')
+                                axis_array[i,j].set(title = 'AX [{0},{1}] - Not Initialized!'.format(i,j))
+                axis_array[0,0].set(title= 'Workspace Signals')
+                canvas = FigureCanvasTkAgg(fig, master=owner)
+                canvas.get_tk_widget().grid(row = position[0],column = position[1],
+                                padx = padX,pady = padY,
+                                columnspan = columnSpan, rowspan = rowSpan,
+                                sticky= sticky)
+                
+                
+                canvas.show()
+                rootApp.signalPlotters[name] = (axis_array,canvas,fig)
+                rootApp.controls[name] = canvas.get_tk_widget()
+
+
         @staticmethod
         def __getColorsList():
                 return [i + j for j in 'o<.' for i in 'bgrcmyk']
